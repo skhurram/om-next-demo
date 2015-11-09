ad72db16ea4dec71b3fddbbbcadf686a595db667
Syed Khurram <khurram@syedkhurram.com>
2015-11-09 19:14:26 +0100

    added figwheel support

--- todomvc/project.clj
+++ todomvc/project.clj
@@ -7,17 +7,18 @@
   :jvm-opts ^:replace ["-Xms512m" "-Xmx512m" "-server"]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.7.145"]
                  [com.datomic/datomic-free "0.9.5206"]
                  [bidi "1.20.3"]
-                 [org.omcljs/om "1.0.0-alpha19"]
+                 [org.omcljs/om "1.0.0-alpha9"]
                  [ring/ring "1.4.0"]
                  [com.cognitect/transit-clj "0.8.281"]
                  [com.cognitect/transit-cljs "0.8.225"]
                  [cljs-http "0.1.30" :exclusions
                   [org.clojure/clojure org.clojure/clojurescript
                    com.cognitect/transit-cljs]]
-                 [com.stuartsierra/component "0.2.3"]]
+                 [com.stuartsierra/component "0.2.3"]
+                 [figwheel-sidecar "0.4.0" :scope "provided"]]
 
   :source-paths ["src/clj" "src/cljs" "src/dev"]
   )
